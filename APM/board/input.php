8c2ce1431f96e0c2e24820d20189ef88094ddf20
HeonjinLee <rkqfus1@gmail.com>
2017-01-23 10:07:32 +0900

    Update input.php

--- APM/board/input.php
+++ APM/board/input.php
@@ -4,9 +4,10 @@
 	<meta charset="utf-8"/>
 </head>
 <body>
-<form action="./process.php?mode=insert" method="POST">
+<form action="./process.php?mode=insert" method="POST" ENCTYPE="multipart/form-data">
 <p>제목 : <input type="text" name="title"></p>
 <p>본문 : <textarea name="des" id="" cols="30" rows="10"></textarea></p>
+<p>file upload : <input type="file" name="fileUpload" id="fileUpload" size="20"></p>
 <p><input type="submit"/></p>
 </form>
 </body>
