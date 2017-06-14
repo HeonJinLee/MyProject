6df4ae5cf9dbe69b5bfdcd4220d0c12c6263b520
HeonjinLee <rkqfus1@gmail.com>
2017-06-14 10:35:07 +0900

    raspberry
    raspberry

---
+++ Study/easy_raspberry.py
@@ -0,0 +1,188 @@
+﻿#센서 확인
+
+import RPi.GPIO as GPIO 	#다목적 입력출력
+import time
+GPIO.setmode(GPIO.BCM)		#p05번 같은 번호로 사용할때 BCM모드로 사용
+GPIO.setup(5,GPIO.OUT)		#p05번을 쓸거다. GPIO.OUT 출력을 위해
+GPIO.output(5,True)		#불이 안들어오게 함
+GPIO.output(5,False)		#불이 들어오게 함
+
+
+
+
+
+
+#불이 5번 깜박 깜박 하게함.
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5,GPIO.OUT)
+for i in range(1,6):
+	try:
+		GPIO.output(5,False)
+		time.sleep(1)
+		GPIO.output(5,True)
+		time.sleep(4)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+
+
+
+
+
+# 전구 두개가 번갈아 가면서 깜빡
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(6,GPIO.OUT)
+while(1):
+	try:
+		GPIO.output(6,False)
+		time.sleep(1)
+		GPIO.output(6,True)
+		GPIO.output(5,False)
+		time.sleep(1)
+		GPIO.output(5,True)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+
+
+
+
+
+
+
+#P26에 꽂은 버튼을 누르면 불이 들어옴
+
+import RPi.GPIO as GPIO	#라즈베리파이 입출력 라이브러리를 파이썬에 가져 쓰겠다
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.IN)
+GPIO.setup(27,GPIO.IN)
+GPIO.setup(5,GPIO.OUT)
+off=1
+on=0
+try:
+	while(1):
+		if GPIO.input(26)==on:
+			GPIO.output(5,on)
+		else:
+			GPIO.output(5,off)
+			time.sleep(0.2)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+
+
+
+
+
+
+
+#27번과 26번 버튼을 누르면 5번 불과 6번 불이 번갈아 가며 나옴.
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.IN)
+GPIO.setup(27,GPIO.IN)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(6,GPIO.OUT)
+off=1
+on=0
+
+def my_callback(channel):
+	if GPIO.input(27)==on:
+		print("switch 2 on")
+		GPIO.output(5,on)
+		GPIO.output(6,off)
+	elif GPIO.input(26)==on:
+		print("switch 1 on")
+		GPIO.output(5,off)
+		GPIO.output(6,on)
+
+GPIO.add_event_detect(27,GPIO.FALLING,callback=my_callback)
+GPIO.add_event_detect(26,GPIO.FALLING,callback=my_callback)
+
+while(1):
+	try:
+		time.sleep(0.01)
+	except KeyboardInterrupt:
+		GPIO.remove_event_detect(27)
+		GPIO.remove_event_detect(26)
+		GPIO.cleanup()
+
+
+
+
+
+
+
+#24번에 연결된 터치 센서를 누르면 5번 불이 들어옴
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5,GPIO.OUT)
+
+on=0
+off=1
+pin_num=24
+
+GPIO.setup(pin_num,GPIO.IN)
+try:
+	while True:
+		pad_pressed = GPIO.input(pin_num)
+
+		if pad_pressed:
+			print("Touch")
+			GPIO.output(5,on)
+		else:
+			GPIO.output(5,off)
+	
+		time.sleep(0.1)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+
+
+
+
+
+
+#24번에 연결된 터치센서 터치를 하면 23번 불이 들어옴
+
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+pin_num=24
+LED=23
+GPIO.setup(pin_num, GPIO.IN)
+GPIO.setup(LED,GPIO.OUT,initial=GPIO.LOW)
+
+try:
+	while True:
+		pad_pressed = GPIO.input(pin_num)
+
+		if pad_pressed:
+			print("Touch")
+			GPIO.output(LED,False)
+		else:
+			GPIO.output(LED,True)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+
+
+
+
+
+
+
---
+++ Study/middle_raspberry.py
@@ -0,0 +1,262 @@
+﻿#소음측정
+
+#5V, GND, 21
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+noise_pin=21
+print("noise sencor test")
+GPIO.setup(noise_pin, GPIO.IN)
+try:
+	i=0
+	while(1):
+		noise_input=GPIO.input(noise_pin)
+		if(noise_input==1):
+			print("noise_checkint every 500ms = %d normal" % noise_input)
+		elif(noise_input==0):
+			i+=1
+			print("repeat %d noise happened = %d time = %s"%(i, noise_input, time.strftime("%H ;%M; %S")))
+			time.sleep(0.5)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+
+
+
+
+
+
+
+
+
+#초음파 거리 센서 연결
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+low = 0
+high = 1
+trig = 21
+echo = 20
+disatance1=[0,0,0,0]
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+GPIO.output(trig, low)
+
+def distance_check():
+	GPIO.output(trig, high)
+	time.sleep(0.00001)
+	GPIO.output(trig, low)
+	stop = 0
+	start = 0
+	while GPIO.input(echo)==0:			#echo가 high되는 시점을 기다림
+		start = time.time()
+	while GPIO.input(echo)==1:			#echo가 low되는 시점을 기다림
+		stop = time.time()
+	duration = stop - start
+	distance = (duration * 340 * 100)/2		#소리의 속도 : 340m/s
+						#340cm로 변환 후 편도거리 계산
+	return distance
+
+def average_disatance():
+	distance=0
+	for i in range(0,4):
+		distance1[i]=distance_check()
+		time.sleep(0.1)
+	for i in range(0,4):
+		distance += disatance1[i]
+	distance = distance / 4
+	return distance
+
+try :
+	i=0
+	while 1:
+		result_distance = average_distance()
+		print("repeat = %d distance = %.1f cm" % (i, result_distance)
+		time.sleep(1)
+		i+=1
+except KeyboardInterrupt:
+	GPIO.cleanup()
+
+
+
+
+
+
+
+
+#온/습도 측정
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+class DHT11:
+	pin = 0
+	temp = -1
+	humi = -1
+
+	def __init__(self, pin, temp=-1, humi=-1):
+		self.temp = temp
+		self.humi = humi
+		self.pin = pin
+
+	def read(self):
+		GPIO.setup(self.pin, GPIO.OUT)
+		GPIO.output(self.pin, GPIO.HIGH)
+		time.sleep(0.05)
+		GPIO.output(self.pin, GPIO.LOW)
+		time.sleep(0.018)
+		GPIO.setup(self.pin, GPIO.IN, GPIO.PUD_UP)
+		data = self.collect_input()
+		pull_up_lengths = self.parse_data_pull_up_lengths(data)
+		if len(pull_up_lengths) != 40:
+			result = 0
+			self.temp=0
+			self.humi=0
+			return result
+
+		bits = self.calculate_bits(pull_up_lengths)
+		the_bytes = self.bits_to_bytes(bits)
+		checksum = self.calculate_checksum(the_bytes)
+		if the_bytes[4] != checksum:
+			result = 0
+			self.temp = 0
+			self.humi = 0
+			return result
+		result=1
+		self.temp = the_bytes[2]
+		self.humi = the_bytes[0]
+		return result
+
+		#온도 센서에 1비트씩 100번 읽음
+	def collect_input(self):
+		unchanged_count = 0
+		max_unchanged_count = 100
+		last = -1
+		data = []
+		while 1:
+			current = GPIO.input(self.pin)
+			data.append(current)
+			if last != current:
+				unchanged_count = 0
+				last = current
+			else:
+				unchanged_count += 1
+				if unchanged_count > max_unchanged_count:
+					break
+		return data
+
+	def parse_data_pull_up_lengths(self. data):
+		STATE_INIT_PULL_DOWN = 1
+		STATE_INIT_PULL_UP = 2
+		STATE_DATA_FIRST_PULL_DOWN = 3
+		STATE_DATA_PULL_UP = 4
+		STATE_DATA_PULL_DOWN = 5
+		state = STATE_INIT_PULL_DOWN
+		lengths = []
+		current_length = 0
+		for i in range(len(data)):
+			current = data[i]
+			current_length += 1
+			if state == STATE_INIT_PULL_DOWN:
+				if current ==GPIO.LOW:
+					state = STATE_INIT_PULL_UP
+					continue
+				else:
+					continue
+
+			if state == STATE_INIT_PULL_UP:
+				if current = GPIO.HIGH:
+					state = STATE_DATA_FIRST_PULL_DOWN
+					continue
+				else:
+					continue
+				
+			if state == STATE_DATA_FIRST_PULL_DOWN:
+				if current == GPIO.LOW:
+					state = STATE_DATA_PULL_UP
+					continue
+				else:
+					continue
+
+			if state == STATE_DATA_PULL_UP:
+				if current == GPIO.HIGH:
+					current_length = 0
+					state = STATE_DATA_PULL_DOWN
+					continue
+				else:
+					continue
+
+			if state == STATE_DATA_PULL_DOWN:
+				if current == GPIO.LOW:
+					lengths.append(current_length)
+					state = STATE_DATA_PULL_UP
+					continue
+				else:
+					continue
+		return lengths
+
+	def calculate_bits(self, pull_up_lengths):
+		bits = []
+		halfway = 15
+		for i in range(0, len(pull_up_lengths)):
+			bit = False
+			if pull_up_lengths[i]>halfway:
+				bit = True
+			bits.append(bit)
+		return bits
+
+	def bits_to_bytes(self,bits):
+		the_bytes = []
+		byte = 0
+		for i in range(0, len(bits)):
+			byte = byte << 1
+			if(bits[i]):
+				byte = byte | 1
+			else : 
+				byte = byte | 0
+			if((i+1) % 8 == 0):
+				the_bytes.append(byte)
+				byte = 0
+		return the_bytes
+
+	def calculate_checksum(self, the_bytes):
+		return the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3] & 255
+
+data = DHT11(pin=4)
+
+while 1:
+	result = data.read()				#데이터 읽기
+	if result:
+		print("Temperature : %d C" % data.temp)	#온도 출력
+		print("Humidity : %d %%" % data.humi)	#습도 출력
+	time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
